b732e0032dfef7f49a56cf298d14696169e79a8b
Ben Howdle <benhowdle89@gmail.com>
2018-03-08 12:58:30 +0000

    rwd

--- src/js/components/login.jsx
+++ src/js/components/login.jsx
@@ -6,7 +6,7 @@ const Login = ({ authenticate }) => {
         <div className="splash">
             <h1 className="logo h-100 center">Top Spo<i className="logo-arrow">&uarr;</i></h1>
         </div>
-        <div className="deets max-width-1 pt4 mx-auto  items-center flex-column">
+        <div className="deets max-width-1 pt4 mx-auto">
             <div className="intro mb2">
                 <h2>Find the tracks you've added most to your playlists</h2>
             </div>
