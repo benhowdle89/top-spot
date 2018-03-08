ff158fed5f2b77f569d6d1a592db7824d3208626
Ben Howdle <benhowdle89@gmail.com>
2018-03-08 12:08:54 +0000

    footer and bg scroller

---
+++ src/js/components/footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const Footer = () => {
+    return <div className="footer absolute p2">
+        <p className="bold"><a href="http://benhowdle.im">Ben Howdle</a></p>
+    </div>
+}
+export default Footer
--- src/js/components/login.jsx
+++ src/js/components/login.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import Footer from './footer'
 import Button from './button'
 const Login = ({ authenticate }) => {
-    return <div className="login max-width-3 mx-auto flex justify-center items-center h-100">
-        <div className="intro">
-            <h1 className="logo mb1">Top <span>Spo</span><i className="logo-arrow">&uarr;</i></h1>
-            <h2>Find the tracks you've added most to your playlists</h2>
+    return <div className="login h-100">
+        <div className="splash">
+            <h1 className="logo h-100 center">Top Spo<i className="logo-arrow">&uarr;</i></h1>
         </div>
-        <Button onClick={authenticate} text="Log in with" />
+        <div className="deets max-width-1 pt4 mx-auto  items-center flex-column">
+            <div className="intro mb2">
+                <h2>Find the tracks you've added most to your playlists</h2>
+            </div>
+            <Button onClick={authenticate} text="Log in with" />
+        </div>
+        <Footer />
     </div>
 }
 export default Login
