10fbde40c3817d1ca1cc48c6ef9d7616e0e4e454
Ben Howdle <benhowdle89@gmail.com>
2018-03-08 16:47:24 +0000

    Update share.jsx

--- src/js/components/share.jsx
+++ src/js/components/share.jsx
@@ -3,7 +3,7 @@ import Button from './button'
 const Share = ({ url }) => {
     const encoded = {
         url: encodeURIComponent(url),
-        text: 'Check out my 🔝 Top Spot playlist on Spotify, created with https://top-spot.stream'
+        text: 'Check out my Top Spot playlist on Spotify, created with https://top-spot.stream'
     }
     return <div className="share max-width-3 mx-auto flex justify-center items-center h-100">
         <h1 className="logo"><a href="/">Top <span>Spo</span><i className="logo-arrow">&uarr;</i></a></h1>
